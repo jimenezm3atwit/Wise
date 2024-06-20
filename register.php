b43391b212a058174e4facc239543d4c06960bba
jonahfiske <fiskej@wit.edu>
2024-06-20 13:53:05 -0400

    Email Symbol added

--- register.php
+++ register.php
@@ -78,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
             <div class="input-box">
                 <input type="text" name="username" placeholder="Enter Email" required>
-                <i class='bx bxs-user-account'></i>
+                <i class='bx bx-envelope'></i>
             </div>
             <div class="input-box">
                 <input type="password" name="password1" placeholder="Enter Password" id="password1" required>
