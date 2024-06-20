7e362eed89a551110cf5d822ed9467b2d248b059
jonahfiske <fiskej@wit.edu>
2024-06-20 15:30:27 -0400

    more api calls

--- login.php
+++ login.php
@@ -50,7 +50,7 @@
                     <h1>WeatherWise Login</h1>
                     <div class="input-box">
                         <input type="text" name="username" placeholder="Email" required aria-label="Username">
-                        <i class='bx bxs-user'></i>
+                        <i class='bx bxs-envelope'></i>
                     </div>
                     <div class="input-box">
                         <input type="password" name="password" placeholder="Password" required aria-label="Password">
--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const apiKey = '88f85db2138ba99acbf7124931a9527c';
 const weatherContainer = document.getElementById("temperature");
 const city = document.getElementById("city");
 const error = document.getElementById('error');
+const daily = document.getElementById("daily");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("1hrRain");
+const sun = document.getElementById("sun");
 
 const units = 'imperial'; //can be imperial or metric
 let temperatureSymobol = units == 'imperial' ? "°F" : "°C";
@@ -12,7 +16,10 @@ async function fetchWeather() {
         weatherContainer.innerHTML = '';
         error.innerHTML = '';
         city.innerHTML = '';
-
+        daily.innerHTML = '';
+        humidity.innerHTML = '';
+        wind.innerHTML = '';
+        sun.innerHTML = '';
 
         const cnt = 1;
         const cityInputtedByUser = document.getElementById("input").value;
@@ -29,13 +36,35 @@ async function fetchWeather() {
             return;
         }
 
+        sunrise = convertUnix(data.sys.sunrise);
+        sunset = convertUnix(data.sys.sunset);
+
         // Display city name based on latitude and longitude
-        city.innerHTML = `City: ${cityInputtedByUser}`;
-        weatherContainer.innerHTML = `Temperature: ${data.main.temp}`
+        city.innerHTML = `City: ${data.name}`;
+        weatherContainer.innerHTML = `Temperature: ${data.main.temp}°F | Feels Like: ${data.main.feels_like}°F`;
+        daily.innerHTML = `Max Temp: ${data.main.temp_max}°F | Min Temp: ${data.main.temp_min}°F`;
+        humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
+        wind.innerHTML = `Wind Speed: ${data.wind.speed} MPH | Wind Direction: ${data.wind.deg}°`;
+        sun.innerHTML = `Sunrise: ${sunrise}AM | Sunset: ${sunset}PM`;
 
     } catch (error) {
         console.log(error);
     }
+}
+
+function convertUnix(unixTimestamp) {
+    const date = new Date(unixTimestamp * 1000);
+
+    // Use methods of the Date object to get readable date and time
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Months are zero-based
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+
+    const formattedTime = `${hours}:${minutes}`;
+    return formattedTime;
 }
 
 function initMap() {
