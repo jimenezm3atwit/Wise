bfb57e6fdce802b177819be01d96ffa9fdd58d41
Jimenezm3atwit <jimenezm3@wit.edu>
2024-06-28 15:39:16 -0400

    kk

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $conn->close();
     <div class="container">
         <aside class="sidebar">
             <ul>
-                <li><a href="index.php">Home</a></li>
+
                 <li><a href="#">Search</a></li>
                 <li><a href="#">Explore</a></li>
                 <li><a href="#">Notifications</a></li>
